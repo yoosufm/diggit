3b6b3a15c32cbee3415216ff9c2b1b39671d6c61
maktoum <mak83826@gmail.com>
2017-01-28 08:51:33 +0530

    ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,g

--- src/test/java/com/diggit/qa/test/analytics/TestAnalytics.java
+++ src/test/java/com/diggit/qa/test/analytics/TestAnalytics.java
@@ -5,9 +5,18 @@ import com.diggit.qa.common.EmailUtil;
 import com.diggit.qa.common.Errors;
 import com.diggit.qa.page.imd.AnalyticPage;
 import com.diggit.qa.util.TestBase;
+import com.google.gson.JsonObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.Date;
 
 /**
  * Created by yoosufm on 12/18/16.
@@ -23,10 +32,25 @@ public class TestAnalytics extends TestBase {
 
     @Test
     public void testLoginAnalytic(){
+        JsonObject jsonReport = new JsonObject();
+
+        jsonReport.addProperty("email", "yoosuf@moogilu.com");
+        jsonReport.addProperty("password", "hafsa2210");
+        jsonReport.addProperty("role_id", "1");
+
+
         try {
-            analyticPage.login("yoosuf@moogilu.com", "hafsa2210", "Super Admin");
-            analyticPage.moveToAnalytic();
-            analyticPage.search("war");
+            HttpClient client = new DefaultHttpClient();
+
+            HttpGet post = new HttpGet(Constant.MANAGEMENT_HOME);
+            System.out.println(jsonReport);
+
+            HttpResponse response = null;
+            response = client.execute(post);
+
+            HttpEntity entity = response.getEntity();
+            String responseString = EntityUtils.toString(entity, "UTF-8");
+            String status =  response.getStatusLine().getReasonPhrase();
         }catch (Exception e){
             String emailBody  = "Production Analytic Is Not Accessible";
             EmailUtil.send(emailBody, emailBody, Constant.TO_LIST);
